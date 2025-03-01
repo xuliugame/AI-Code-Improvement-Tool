0dc91d2458d2c5f6b12587fa9266f1216b5def6a
xuliugame <xuliugame@gmail.com>
2025-03-01 17:18:29 -0500

    remove frontend test

--- frontend/tests/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../src/App';
-
-test('renders the main page', () => {
-  render(<App />);
-  expect(screen.getByText(/Code Improvement Tool/i)).toBeInTheDocument();
-});
